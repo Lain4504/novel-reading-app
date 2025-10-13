c4ee51e415ce11fe3cac15222bace45517c6c0d2
khangnld <163306744+khangnld@users.noreply.github.com>
2025-10-13 15:06:20 +0700

    comment + noti

--- backend/src/main/kotlin/com/miraimagiclab/novelreadingapp/dto/request/NotificationUpdateRequest.kt
+++
@@ -1,7 +0,0 @@
-package com.miraimagiclab.novelreadingapp.dto.request
-
-data class NotificationUpdateRequest(
-    val title: String? = null,
-    val message: String? = null,
-    val read: Boolean? = null
-)
